46d1b4628764664b00614c41d5ac09a56fbe9fd6
Vlada B <vlada.bessikalo@gmail.com>
2024-10-20 14:49:23 +0200

    create list of questions

--- src/data.js
+++ src/data.js
@@ -13,48 +13,205 @@ export const quizData = {
   currentQuestionIndex: 0,
   // the questions in the quiz
   questions: [
-    {
-      text: 'What are the different ways to declare a JS variable?',
-      answers: {
-        a: 'constant, let, variable',
-        b: 'var, const, let, function',
-        c: 'var, let, const',
+      {
+        text: 'What is the capital of the Netherlands?',
+        answers: {
+          a: 'Amsterdam',
+          b: 'Rotterdam',
+          c: 'The Hague',
+        },
+        correct: 'a',
+        selected: null,
+        links: [
+          {
+            text: 'Britannica',
+            href: 'https://www.britannica.com/place/Amsterdam',
+          },
+          {
+            text: 'Holland.com',
+            href: 'https://www.holland.com/global/tourism/destinations/amsterdam.htm',
+          },
+        ],
       },
-      correct: 'c',
-      selected: null,
-      links: [
-        {
-          text: 'javascript.info',
-          href: 'https://javascript.info/variables',
+      {
+        text: 'Which of the following is a traditional Dutch food?',
+        answers: {
+          a: 'Sushi',
+          b: 'Stroopwafel',
+          c: 'Tacos',
         },
-        {
-          text: 'Tyler McGinnis',
-          href: 'https://ui.dev/var-let-const/',
-        },
-      ],
-    },
-    {
-      text: 'What does `typeof` do?',
-      answers: {
-        a: 'changes the type of a primitive value',
-        b: 'returns a string describing the type of a value',
-        c: 'determines if a value is primitive',
-        d: 'can tell the difference between arrays and objects',
+        correct: 'b',
+        selected: null,
+        links: [
+          {
+            text: 'Dutch Review',
+            href: 'https://dutchreview.com/culture/food/the-ultimate-guide-to-dutch-food/',
+          },
+          {
+            text: 'Expatica',
+            href: 'https://www.expatica.com/nl/lifestyle/food-drink/dutch-food-100-things-101947/',
+          },
+        ],
       },
-      correct: 'b',
-      selected: null,
-      links: [
-        {
-          text: 'javascript.info',
-          href: 'https://javascript.info/types#type-typeof',
+      {
+        text: 'What color is typically associated with the Dutch royal family?',
+        answers: {
+          a: 'Red',
+          b: 'Orange',
+          c: 'Blue',
         },
-        {
-          text: 'MDN',
-          href:
-            'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof',
+        correct: 'b',
+        selected: null,
+        links: [
+          {
+            text: 'Netherlandsandyou.nl',
+            href: 'https://www.netherlandsandyou.nl/about-the-netherlands/royal-house',
+          },
+          {
+            text: 'Royal House of the Netherlands',
+            href: 'https://www.royal-house.nl/',
+          },
+        ],
+      },
+      {
+        text: 'Which country borders the Netherlands to the south?',
+        answers: {
+          a: 'Belgium',
+          b: 'Germany',
+          c: 'France',
         },
-      ],
-    },
-    // Add more questions here
-  ],
+        correct: 'a',
+        selected: null,
+        links: [
+          {
+            text: 'World Atlas',
+            href: 'https://www.worldatlas.com/maps/netherlands',
+          },
+          {
+            text: 'Britannica',
+            href: 'https://www.britannica.com/place/Netherlands',
+          },
+        ],
+      },
+      {
+        text: 'Which city is known for its international court of justice?',
+        answers: {
+          a: 'Rotterdam',
+          b: 'Amsterdam',
+          c: 'The Hague',
+        },
+        correct: 'c',
+        selected: null,
+        links: [
+          {
+            text: 'International Court of Justice',
+            href: 'https://www.icj-cij.org/en',
+          },
+          {
+            text: 'The Hague',
+            href: 'https://www.denhaag.com/en/international-city-of-peace-and-justice',
+          },
+        ],
+      },
+      {
+        text: 'Which of these is a major river flowing through the Netherlands?',
+        answers: {
+          a: 'Thames',
+          b: 'Rhine',
+          c: 'Seine',
+        },
+        correct: 'b',
+        selected: null,
+        links: [
+          {
+            text: 'Britannica',
+            href: 'https://www.britannica.com/place/Rhine-River',
+          },
+          {
+            text: 'World Atlas',
+            href: 'https://www.worldatlas.com/rivers/rhine-river.html',
+          },
+        ],
+      },
+      {
+        text: 'What is the name of the Dutch national airline?',
+        answers: {
+          a: 'Lufthansa',
+          b: 'KLM',
+          c: 'Air France',
+        },
+        correct: 'b',
+        selected: null,
+        links: [
+          {
+            text: 'KLM Official Site',
+            href: 'https://www.klm.com/home/nl/en',
+          },
+          {
+            text: 'SkyTeam',
+            href: 'https://www.skyteam.com/en/about/airlines/klm',
+          },
+        ],
+      },
+      {
+        text: 'What is the national flower of the Netherlands?',
+        answers: {
+          a: 'Tulip',
+          b: 'Rose',
+          c: 'Lily',
+        },
+        correct: 'a',
+        selected: null,
+        links: [
+          {
+            text: 'Holland.com',
+            href: 'https://www.holland.com/global/tourism/interests/traditional/tulips.htm',
+          },
+          {
+            text: 'Dutch Review',
+            href: 'https://dutchreview.com/culture/why-tulips-are-the-national-flower-of-the-netherlands/',
+          },
+        ],
+      },
+      {
+        text: 'What is the main language spoken in the Netherlands?',
+        answers: {
+          a: 'English',
+          b: 'French',
+          c: 'Dutch',
+        },
+        correct: 'c',
+        selected: null,
+        links: [
+          {
+            text: 'Holland.com',
+            href: 'https://www.holland.com/global/tourism/information/general/language.htm',
+          },
+          {
+            text: 'Expatica',
+            href: 'https://www.expatica.com/nl/living/integration/language-in-the-netherlands-105680/',
+          },
+        ],
+      },
+      {
+        text: 'Which famous Dutch artist painted "The Starry Night"?',
+        answers: {
+          a: 'Vincent van Gogh',
+          b: 'Rembrandt',
+          c: 'Piet Mondrian',
+        },
+        correct: 'a',
+        selected: null,
+        links: [
+          {
+            text: 'Van Gogh Museum',
+            href: 'https://www.vangoghmuseum.nl/en',
+          },
+          {
+            text: 'The Art Story',
+            href: 'https://www.theartstory.org/artist/van-gogh-vincent/',
+          },
+        ],
+      }
+    ]
 };
