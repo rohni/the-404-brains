de4092c689573bbf5683469c1908a99faa183e19
Vlada B <vlada.bessikalo@gmail.com>
2024-10-21 10:18:47 +0200

    update questions list

--- src/data.js
+++ src/data.js
@@ -13,205 +13,215 @@ export const quizData = {
   currentQuestionIndex: 0,
   // the questions in the quiz
   questions: [
-      {
-        text: 'What is the capital of the Netherlands?',
-        answers: {
-          a: 'Amsterdam',
-          b: 'Rotterdam',
-          c: 'The Hague',
-        },
-        correct: 'a',
-        selected: null,
-        links: [
-          {
-            text: 'Britannica',
-            href: 'https://www.britannica.com/place/Amsterdam',
-          },
-          {
-            text: 'Holland.com',
-            href: 'https://www.holland.com/global/tourism/destinations/amsterdam.htm',
-          },
-        ],
+    {
+      "text": "What is the capital of the Netherlands?",
+      "answers": {
+        "a": "Amsterdam",
+        "b": "Rotterdam",
+        "c": "The Hague",
+        "d": "Utrecht"
       },
-      {
-        text: 'Which of the following is a traditional Dutch food?',
-        answers: {
-          a: 'Sushi',
-          b: 'Stroopwafel',
-          c: 'Tacos',
+      "correct": "a",
+      "selected": "a",
+      "links": [
+        {
+          "text": "Britannica",
+          "href": "https://www.britannica.com/place/Amsterdam"
         },
-        correct: 'b',
-        selected: null,
-        links: [
-          {
-            text: 'Dutch Review',
-            href: 'https://dutchreview.com/culture/food/the-ultimate-guide-to-dutch-food/',
-          },
-          {
-            text: 'Expatica',
-            href: 'https://www.expatica.com/nl/lifestyle/food-drink/dutch-food-100-things-101947/',
-          },
-        ],
+        {
+          "text": "Holland.com",
+          "href": "https://www.holland.com/global/tourism/destinations/amsterdam.htm"
+        }
+      ]
+    },
+    {
+      "text": "Which of the following is a traditional Dutch food?",
+      "answers": {
+        "a": "Sushi",
+        "b": "Stroopwafel",
+        "c": "Tacos",
+        "d": "Bitterballen"
       },
-      {
-        text: 'What color is typically associated with the Dutch royal family?',
-        answers: {
-          a: 'Red',
-          b: 'Orange',
-          c: 'Blue',
+      "correct": "b",
+      "selected": "b",
+      "links": [
+        {
+          "text": "Dutch Review",
+          "href": "https://dutchreview.com/culture/food/the-ultimate-guide-to-dutch-food/"
         },
-        correct: 'b',
-        selected: null,
-        links: [
-          {
-            text: 'Netherlandsandyou.nl',
-            href: 'https://www.netherlandsandyou.nl/about-the-netherlands/royal-house',
-          },
-          {
-            text: 'Royal House of the Netherlands',
-            href: 'https://www.royal-house.nl/',
-          },
-        ],
+        {
+          "text": "Expatica",
+          "href": "https://www.expatica.com/nl/lifestyle/food-drink/dutch-food-100-things-101947/"
+        }
+      ]
+    },
+    {
+      "text": "What color is typically associated with the Dutch royal family?",
+      "answers": {
+        "a": "Red",
+        "b": "Orange",
+        "c": "Blue",
+        "d": "White"
       },
-      {
-        text: 'Which country borders the Netherlands to the south?',
-        answers: {
-          a: 'Belgium',
-          b: 'Germany',
-          c: 'France',
+      "correct": "b",
+      "selected": "b",
+      "links": [
+        {
+          "text": "Netherlandsandyou.nl",
+          "href": "https://www.netherlandsandyou.nl/about-the-netherlands/royal-house"
         },
-        correct: 'a',
-        selected: null,
-        links: [
-          {
-            text: 'World Atlas',
-            href: 'https://www.worldatlas.com/maps/netherlands',
-          },
-          {
-            text: 'Britannica',
-            href: 'https://www.britannica.com/place/Netherlands',
-          },
-        ],
+        {
+          "text": "Royal House of the Netherlands",
+          "href": "https://www.royal-house.nl/"
+        }
+      ]
+    },
+    {
+      "text": "Which country borders the Netherlands to the south?",
+      "answers": {
+        "a": "Belgium",
+        "b": "Germany",
+        "c": "France",
+        "d": "Luxembourg"
       },
-      {
-        text: 'Which city is known for its international court of justice?',
-        answers: {
-          a: 'Rotterdam',
-          b: 'Amsterdam',
-          c: 'The Hague',
+      "correct": "a",
+      "selected": "a",
+      "links": [
+        {
+          "text": "World Atlas",
+          "href": "https://www.worldatlas.com/maps/netherlands"
         },
-        correct: 'c',
-        selected: null,
-        links: [
-          {
-            text: 'International Court of Justice',
-            href: 'https://www.icj-cij.org/en',
-          },
-          {
-            text: 'The Hague',
-            href: 'https://www.denhaag.com/en/international-city-of-peace-and-justice',
-          },
-        ],
+        {
+          "text": "Britannica",
+          "href": "https://www.britannica.com/place/Netherlands"
+        }
+      ]
+    },
+    {
+      "text": "Which city is known for its international court of justice?",
+      "answers": {
+        "a": "Rotterdam",
+        "b": "Amsterdam",
+        "c": "The Hague",
+        "d": "Utrecht"
       },
-      {
-        text: 'Which of these is a major river flowing through the Netherlands?',
-        answers: {
-          a: 'Thames',
-          b: 'Rhine',
-          c: 'Seine',
+      "correct": "c",
+      "selected": "c",
+      "links": [
+        {
+          "text": "International Court of Justice",
+          "href": "https://www.icj-cij.org/en"
         },
-        correct: 'b',
-        selected: null,
-        links: [
-          {
-            text: 'Britannica',
-            href: 'https://www.britannica.com/place/Rhine-River',
-          },
-          {
-            text: 'World Atlas',
-            href: 'https://www.worldatlas.com/rivers/rhine-river.html',
-          },
-        ],
+        {
+          "text": "The Hague",
+          "href": "https://www.denhaag.com/en/international-city-of-peace-and-justice"
+        }
+      ]
+    },
+    {
+      "text": "Which of these is a major river flowing through the Netherlands?",
+      "answers": {
+        "a": "Thames",
+        "b": "Rhine",
+        "c": "Seine",
+        "d": "Danube"
       },
-      {
-        text: 'What is the name of the Dutch national airline?',
-        answers: {
-          a: 'Lufthansa',
-          b: 'KLM',
-          c: 'Air France',
+      "correct": "b",
+      "selected": "b",
+      "links": [
+        {
+          "text": "Britannica",
+          "href": "https://www.britannica.com/place/Rhine-River"
         },
-        correct: 'b',
-        selected: null,
-        links: [
-          {
-            text: 'KLM Official Site',
-            href: 'https://www.klm.com/home/nl/en',
-          },
-          {
-            text: 'SkyTeam',
-            href: 'https://www.skyteam.com/en/about/airlines/klm',
-          },
-        ],
+        {
+          "text": "World Atlas",
+          "href": "https://www.worldatlas.com/rivers/rhine-river.html"
+        }
+      ]
+    },
+    {
+      "text": "What is the name of the Dutch national airline?",
+      "answers": {
+        "a": "Lufthansa",
+        "b": "KLM",
+        "c": "Air France",
+        "d": "EasyJet"
       },
-      {
-        text: 'What is the national flower of the Netherlands?',
-        answers: {
-          a: 'Tulip',
-          b: 'Rose',
-          c: 'Lily',
+      "correct": "b",
+      "selected": "b",
+      "links": [
+        {
+          "text": "KLM Official Site",
+          "href": "https://www.klm.com/home/nl/en"
         },
-        correct: 'a',
-        selected: null,
-        links: [
-          {
-            text: 'Holland.com',
-            href: 'https://www.holland.com/global/tourism/interests/traditional/tulips.htm',
-          },
-          {
-            text: 'Dutch Review',
-            href: 'https://dutchreview.com/culture/why-tulips-are-the-national-flower-of-the-netherlands/',
-          },
-        ],
+        {
+          "text": "SkyTeam",
+          "href": "https://www.skyteam.com/en/about/airlines/klm"
+        }
+      ]
+    },
+    {
+      "text": "What is the national flower of the Netherlands?",
+      "answers": {
+        "a": "Tulip",
+        "b": "Rose",
+        "c": "Lily",
+        "d": "Daffodil"
       },
-      {
-        text: 'What is the main language spoken in the Netherlands?',
-        answers: {
-          a: 'English',
-          b: 'French',
-          c: 'Dutch',
+      "correct": "a",
+      "selected": "a",
+      "links": [
+        {
+          "text": "Holland.com",
+          "href": "https://www.holland.com/global/tourism/interests/traditional/tulips.htm"
         },
-        correct: 'c',
-        selected: null,
-        links: [
-          {
-            text: 'Holland.com',
-            href: 'https://www.holland.com/global/tourism/information/general/language.htm',
-          },
-          {
-            text: 'Expatica',
-            href: 'https://www.expatica.com/nl/living/integration/language-in-the-netherlands-105680/',
-          },
-        ],
+        {
+          "text": "Dutch Review",
+          "href": "https://dutchreview.com/culture/why-tulips-are-the-national-flower-of-the-netherlands/"
+        }
+      ]
+    },
+    {
+      "text": "What is the main language spoken in the Netherlands?",
+      "answers": {
+        "a": "English",
+        "b": "French",
+        "c": "Dutch",
+        "d": "German"
       },
-      {
-        text: 'Which famous Dutch artist painted "The Starry Night"?',
-        answers: {
-          a: 'Vincent van Gogh',
-          b: 'Rembrandt',
-          c: 'Piet Mondrian',
+      "correct": "c",
+      "selected": "c",
+      "links": [
+        {
+          "text": "Holland.com",
+          "href": "https://www.holland.com/global/tourism/information/general/language.htm"
         },
-        correct: 'a',
-        selected: null,
-        links: [
-          {
-            text: 'Van Gogh Museum',
-            href: 'https://www.vangoghmuseum.nl/en',
-          },
-          {
-            text: 'The Art Story',
-            href: 'https://www.theartstory.org/artist/van-gogh-vincent/',
-          },
-        ],
-      }
-    ]
+        {
+          "text": "Expatica",
+          "href": "https://www.expatica.com/nl/living/integration/language-in-the-netherlands-105680/"
+        }
+      ]
+    },
+    {
+      "text": "Which famous Dutch artist painted 'The Starry Night'?",
+      "answers": {
+        "a": "Vincent van Gogh",
+        "b": "Rembrandt",
+        "c": "Piet Mondrian",
+        "d": "Johannes Vermeer"
+      },
+      "correct": "a",
+      "selected": "a",
+      "links": [
+        {
+          "text": "Van Gogh Museum",
+          "href": "https://www.vangoghmuseum.nl/en"
+        },
+        {
+          "text": "The Art Story",
+          "href": "https://www.theartstory.org/artist/van-gogh-vincent/"
+        }
+      ]
+    }
+  ]  
 };
