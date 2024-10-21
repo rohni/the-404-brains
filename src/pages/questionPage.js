e5aa872f9067ca187a4b68d36637b884c94fca89
Vlada B <vlada.bessikalo@gmail.com>
2024-10-21 22:22:53 +0200

    add progress-bar and some styling

--- src/pages/questionPage.js
+++ src/pages/questionPage.js
@@ -14,16 +14,13 @@ export const initQuestionPage = () => {
   userInterface.innerHTML = '';
 
   const currentQuestion = quizData.questions[quizData.currentQuestionIndex];
-
   const questionElement = createQuestionElement(currentQuestion.text);
 
   userInterface.appendChild(questionElement);
 
-
   // Status bar
   const statusBar = createStatusBar(quizData.currentQuestionIndex + 1, quizData.questions.length)
   userInterface.prepend(statusBar);
-  
 
   const answersListElement = document.getElementById(ANSWERS_LIST_ID);
 
@@ -34,7 +31,7 @@ export const initQuestionPage = () => {
 
   document
     .getElementById(NEXT_QUESTION_BUTTON_ID)
-    .addEventListener('click', nextQuestion);
+    .addEventListener('click', () => nextQuestion(statusBar))
 };
 
 const nextQuestion = (statusBar) => {
@@ -48,17 +45,23 @@ const nextQuestion = (statusBar) => {
 };
 
 const showEndOfTheQuiz = () => {
-  console.log("Create finale page with score");
+  console.log("You have reached the last question!");
   const nextButton = document.getElementById(NEXT_QUESTION_BUTTON_ID);
   nextButton.disabled = true;
 
+  const userInterface = document.getElementById(USER_INTERFACE_ID);
   const finaleMessage = document.createElement('p');
   finaleMessage.textContent = 'Congratulations! You have completed the quiz.'
-  const userInterface = document.getElementById(USER_INTERFACE_ID);
-  const finalResultBtn = document.createElement('a');
+  const finalResultBtn = document.createElement('button');
   finalResultBtn.textContent = 'Show Final Result!';
   finalResultBtn.classList.add(FINAL_RESULT_BUTTON_ID);
+  finalResultBtn.addEventListener('click', showFinalResult);
+
   userInterface.appendChild(finaleMessage);
   userInterface.appendChild(finalResultBtn);
+};
+
+const showFinalResult = () => {
+  console.log("Redirect to Final result page");
 };
  
--- src/views/statusBarView.js
+++ src/views/statusBarView.js
@@ -1,12 +1,32 @@
 export const createStatusBar = (currentQuestion, totalQuestions) => {
     const statusBar = document.createElement('div');
     statusBar.classList.add('status-bar');
-    statusBar.innerHTML = `${currentQuestion}/${totalQuestions}`
+
+    const progressBarContainer = document.createElement('div');
+    progressBarContainer.classList.add('progress-bar-container');
+
+    const progressBar = document.createElement('div');
+    progressBar.classList.add('progress-bar');
+    progressBar.style.width = `${(currentQuestion/totalQuestions) * 100}%`;
+
+    progressBarContainer.appendChild(progressBar);
+    statusBar.appendChild(progressBarContainer);
+
+    const progressText = document.createElement('span');
+    progressText.textContent = `${currentQuestion}/${totalQuestions}`;
+    statusBar.appendChild(progressText);
+
     return statusBar;
 }
 
 // Have to add Score too after Esen's feature
 
-export const updateStatusBar = (statusBarElement, currentQuestion, totalQuestions) => {
-    statusBarElement.innerHTML = `${currentQuestion}/${totalQuestions}`
+export const updateStatusBar = (statusBarElement, currentQuestion, totalQuestions) => {    
+    const progressBar = statusBarElement.querySelector('.progress-bar');
+    const progressBarWidth = (currentQuestion / totalQuestions) * 100;
+
+    progressBar.style.width = `${progressBarWidth}%`;
+
+    const progressText = statusBarElement.querySelector('span');
+    progressText.textContent = `${currentQuestion}/${totalQuestions}`
 }
