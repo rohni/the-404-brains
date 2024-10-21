fd6f4a64497e6d5d054344149617a046f88bfbfa
Vlada B <vlada.bessikalo@gmail.com>
2024-10-21 16:11:17 +0200

    Create status-bar view, add it to the question page

--- src/pages/questionPage.js
+++ src/pages/questionPage.js
@@ -2,10 +2,12 @@ import {
   ANSWERS_LIST_ID,
   NEXT_QUESTION_BUTTON_ID,
   USER_INTERFACE_ID,
+  FINAL_RESULT_BUTTON_ID
 } from '../constants.js';
 import { createQuestionElement } from '../views/questionView.js';
 import { createAnswerElement } from '../views/answerView.js';
 import { quizData } from '../data.js';
+import { createStatusBar, updateStatusBar } from '../views/statusBarView.js';
 
 export const initQuestionPage = () => {
   const userInterface = document.getElementById(USER_INTERFACE_ID);
@@ -16,6 +18,12 @@ export const initQuestionPage = () => {
   const questionElement = createQuestionElement(currentQuestion.text);
 
   userInterface.appendChild(questionElement);
+
+
+  // Status bar
+  const statusBar = createStatusBar(quizData.currentQuestionIndex + 1, quizData.questions.length)
+  userInterface.prepend(statusBar);
+  
 
   const answersListElement = document.getElementById(ANSWERS_LIST_ID);
 
@@ -29,8 +37,24 @@ export const initQuestionPage = () => {
     .addEventListener('click', nextQuestion);
 };
 
-const nextQuestion = () => {
-  quizData.currentQuestionIndex = quizData.currentQuestionIndex + 1;
+const nextQuestion = (statusBar) => {
+  if (quizData.currentQuestionIndex < quizData.questions.length - 1) {
+    quizData.currentQuestionIndex++;
+    updateStatusBar(statusBar, quizData.currentQuestionIndex + 1, quizData.questions.length)
+    initQuestionPage();
+  } else if (quizData.currentQuestionIndex === quizData.questions.length -1) {
+    console.log("Create finale page with score");
+    const nextButton = document.getElementById(NEXT_QUESTION_BUTTON_ID);
+    nextButton.disabled = true;
 
-  initQuestionPage();
+    const finaleMessage = document.createElement('p');
+    finaleMessage.textContent = 'Congratulations! You have completed the quiz.'
+    const userInterface = document.getElementById(USER_INTERFACE_ID);
+    const finalResultBtn = document.createElement('a');
+    finalResultBtn.textContent = 'Show Final Result!';
+    finalResultBtn.classList.add(FINAL_RESULT_BUTTON_ID);
+    userInterface.appendChild(finaleMessage);
+    userInterface.appendChild(finalResultBtn);
+  }
 };
+ 
---
+++ src/views/statusBarView.js
@@ -0,0 +1,12 @@
+export const createStatusBar = (currentQuestion, totalQuestions) => {
+    const statusBar = document.createElement('div');
+    statusBar.classList.add('status-bar');
+    statusBar.innerHTML = `${currentQuestion}/${totalQuestions}`
+    return statusBar;
+}
+
+// Have to add Score too after Esens feature
+
+export const updateStatusBar = (statusBarElement, currentQuestion, totalQuestions) => {
+    statusBarElement.innerHTML = `${currentQuestion}/${totalQuestions}`
+}
