93ebae05cfa5009f4e6a22c1678caab1469c8482
EsenKaratas <esennurkaratass@gmail.com>
2024-10-25 11:10:39 +0200

    fixed question-box

--- src/views/finishPageView.js
+++ src/views/finishPageView.js
@@ -12,7 +12,7 @@ export const createFinishPageElement = () => {
     : '<div class="expert-message">Great job! You are definitely a local from the Netherlands!!!</div>';
 
     const image = correctAnswersCount < 5
-    ? 'https://giphy.com/embed/3OhXBaoR1tVPW'
+    ? 'https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExcG42M284dnViZmFpenhscjlxbG1wYTNvOWl6OXYzZjY3ZDdidWV0cyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3OhXBaoR1tVPW/giphy.gif'
     : correctAnswersCount < 8
     ? 'https://c.tenor.com/MKV-2xMdq28AAAAC/tenor.gif'
     : 'https://media.giphy.com/media/URurgslg2xhvYueMx1/giphy.gif'; 
@@ -21,6 +21,7 @@ export const createFinishPageElement = () => {
     finishPageElement.innerHTML = String.raw`
         <h1>Well done, ${userName}! </h1>
         <img src="${image}" alt="Finish image"/>
+        <div> You have answered ${correctAnswersCount} questions correctly! </div>
         <div> The number of questions you have skipped is ${skipAnswer} </div>
         ${resultMessage}
         <button id="${RESTART_QUIZ_BUTTON_ID}">Try again!</button>
